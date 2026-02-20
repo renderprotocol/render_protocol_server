f27a4582f0fbce7b3c0a7c98cd2ad56e4e688c40
Arindam Karmakar <connect@karindam.in>
2026-02-20 21:49:56 +0530

    Updated mock render service

--- lib/mocks/rp_render_service_mock.dart
+++ lib/mocks/rp_render_service_mock.dart
@@ -11,19 +11,69 @@ class RPRenderServiceMock extends RPRenderServiceBase {
     grpc.ServiceCall call,
     RPFetchRenderTreeRequest request,
   ) async {
+    final textStyle = RPTextStyle();
+    textStyle.fontSize = 18.0;
+    textStyle.fontWeight = .RP_FONT_WEIGHT_BOLD;
+
     final text = RPText();
     text.value = "Hello world from RP!";
+    text.style = textStyle;
+
+    final opacity = RPOpacity();
+    opacity.value = 0.7;
+    opacity.child = text.makeWidget();
 
     final image = RPImage();
     image.url = "https://picsum.photos/300/200";
 
-    final column = RPColumn();
-    column.children.add(text.makeWidget());
-    column.children.add(image.makeWidget());
+    final bgColor = RPColor();
+    bgColor.red = 0;
+    bgColor.green = 128;
+    bgColor.blue = 128;
+
+    final clipShape = RPClipShape();
+    clipShape.roundedRectangle = .new()..radius = 12.0;
+    final clip = RPClip();
+    clip.shape = clipShape;
+    clip.type = .RP_CLIP_TYPE_ANTI_ALIAS;
+    clip.child = image.makeWidget();
+
+    final row = rpRow(
+      children: [
+        clip.makeWidget(),
+        clip.makeWidget(),
+      ],
+    );
+
+    final column = rpColumn(
+      children: [
+        opacity.makeWidget(),
+        row.makeWidget(),
+      ],
+      alignment: .RP_AXIS_ALIGNMENT_LEADING,
+    );
+
+    final card = RPCard();
+    card.child = column.makeWidget();
+    card.backgroundColor = rpColorTeal;
+
+    final box = RPSizeBox();
+    box.height = 200.0;
+    box.child = card.makeWidget();
+
+    final lazyList = RPLazyList();
+    lazyList.spacing = 2.5;
+    lazyList.separator = rpDivider(color: rpColorGray).makeWidget();
+    lazyList.axis = .RP_AXIS_VERTICAL;
+    for (int i = 0; i < 1000; i++) {
+      lazyList.children.add(box.makeWidget());
+    }
+
+    final component = lazyList.makeWidget().withPadding(8.0);
 
     final response = RPFetchRenderTreeResponse();
     response.id = _uuid.v1();
-    response.view = column.makeWidget();
+    response.view = component;
     return response;
   }
 }
